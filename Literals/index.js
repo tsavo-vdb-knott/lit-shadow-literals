35137b06ae25e6b16a5ed6fbb4450cd3fd8ff512
Holden Hicks <holden.hicks@mesh-hq.com>
2019-02-20 14:34:07 -0500

    New Dark theme shadows

--- Literals/index.js
+++ Literals/index.js
@@ -18,14 +18,14 @@ export const noShadow = css`
 `;
 
 export const smallShadowDarkTheme = css`
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5), 0 6px 6px rgba(0, 0, 0, 0.25);
 `;
 
 export const mediumShadowDarkTheme = css`
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.5), 0 10px 10px rgba(0, 0, 0, 0.5);
 `;
 
 export const largeShadowDarkTheme = css`
-  box-shadow: 0 15px 30px 0 rgba(0, 0, 0, 0.11),
-    0 5px 15px 0 rgba(0, 0, 0, 0.08);
+  box-shadow: 0 19px 38px rgba(0,0,0,0.5), 0 15px 12px rgba(0,0,0, 0.5);
+}
 `;
